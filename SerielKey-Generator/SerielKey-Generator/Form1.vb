c0a976084682cedad231e69af84df2c53690d8c5
David Hilberath <David-Hilberath@hotmail.de>
2016-11-10 15:38:28 +0100

    Komplett fertig erstellt

---
+++ SerielKey-Generator/SerielKey-Generator/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    'KfhgF-HFSDH-JHFHD-5F8RS
+    Dim PasswortSet As String = "ABCDEFGHICKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    Dim rnd As New System.Random
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'MsgBox(Len(PasswortSet))
+        TextBox1.Clear()
+        Dim tmp As String = ""
+        For i As Integer = 0 To 22
+            If i = 5 Or i = 11 Or i = 17 Then
+                tmp = tmp & "-"
+            Else
+                Dim x As Integer = rnd.Next(0, 61)
+                tmp = tmp & PasswortSet.Chars(x)
+            End If
+        Next
+        TextBox1.Text = tmp
+    End Sub
+End Class
